619dbadc4228c9e1b21294f0bb4a6b570aac16cb
Marcus Enblom <marcusenblom@gmail.com>
2021-01-18 14:33:07 +0100

    NaN on profile

--- client/src/components/userProfile/profileStatWinRatio/profileStatWinRatio.tsx
+++ client/src/components/userProfile/profileStatWinRatio/profileStatWinRatio.tsx
@@ -10,7 +10,7 @@ export default function ProfileStatWinRatio(props: IProfileStatWinRatio){
     return(
         <div className="user-profile-stat-win-ratio-percentage">
             <div className={"c100 p" + props.value.match(/[^.]*$/g)[0] + " size main-color"}>
-                <span>{props.value}</span>
+                {props.value === "NaN" ? <span>...</span> : <span>{props.value}</span>}
                 <div className="slice">
                     <div className="bar"></div>
                     <div className="fill"></div>
